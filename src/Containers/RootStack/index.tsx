943cb1b1fe6110a00f888ed7f312bbb2872ef60e
hzfeng <hzfeng@Ctrip.com>
2019-07-22 11:32:10 +0800

    update

--- src/Containers/RootStack/index.tsx
+++ src/Containers/RootStack/index.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import {AsyncStorage, StatusBar} from "react-native";
+import {StatusBar} from "react-native";
 import {createAppContainer, createStackNavigator} from "react-navigation";
 import Root from "@/Containers/Root";
 import CookieGet from "@/Containers/CookieGet";
 import Context from "@/Context";
 import {useState, useEffect} from "react";
+import AsyncStorage from '@react-native-community/async-storage';
 
 const AppNavigator = createStackNavigator({
     Root: {
